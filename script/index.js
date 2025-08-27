707c0f5dd457e4cb77023c9fc13fe1a6daffcf8b
Rakib Hossain <rizviofficialteam@gmail.com>
2025-08-27 10:22:52 +0600

    one button functionality and half website

---
+++ script/index.js
@@ -0,0 +1,98 @@
+let callingData = [];
+// এক বৃদ্ধি করার ফাংশন
+function countIncrease(id) {
+  let heartCount = parseInt(document.getElementById(id).innerText);
+  document.getElementById(id).innerText = heartCount + 1;
+}
+// কল নাম্বার কপি
+function copyBtn(id) {
+  let text = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(text);
+
+  alert("নাম্বার কপি হয়েছে: " + text);
+}
+// কল করার ফাংশন
+function callingBtn(id1, id2, coinC) {
+  const callHeading = document.getElementById(id1).innerText;
+  const num = document.getElementById(id2).innerText;
+
+  let coinCount = parseInt(document.getElementById(coinC).innerText);
+  if (coinCount >= 20) {
+    alert("Calling " + callHeading + " " + num);
+    coinCount = coinCount - 20;
+    document.getElementById(coinC).innerText = coinCount;
+
+    let data = {
+      name: callHeading,
+      number: num,
+      date: new Date().toLocaleTimeString(),
+    };
+    callingData.push(data);
+    console.log(callingData);
+    // ক্লিয়ার বাটন এ ক্লিক
+    document.getElementById("clear-btn").addEventListener("click", function () {
+      document.getElementById("call-history-container").innerHTML = "";
+      callingData = [];
+    });
+  } else {
+    alert("আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+  }
+}
+
+// হার্ট বাটন এ ক্লিক
+document.getElementById("heart-btn").addEventListener("click", function () {
+  countIncrease("heart-count");
+  //   let heartCount = parseInt(document.getElementById("heart-count").innerText);
+  //   heartCount = heartCount + 1;
+  //   console.log(heartCount);
+  //   document.getElementById("heart-count").innerText = heartCount + 1;
+});
+
+// কপি বাটন এ ক্লিক
+document.getElementById("copy-btn").addEventListener("click", function () {
+  copyBtn("national-calling-number");
+  // alert('নাম্বার কপি হয়েছে')
+  countIncrease("copy-count");
+});
+
+// কল বাটন এ ক্লিক
+document.getElementById("calling-btn").addEventListener("click", function () {
+  callingBtn("calling-heading", "national-calling-number", "coin-count");
+  allCallBtn("call-history-container")
+});
+
+// let callHistoryContainer = document.getElementById("call-history-container");
+//   callHistoryContainer.innerText = "";
+//   for (const data of callingData) {
+//     let div = document.createElement("div");
+//     div.innerHTML = `
+//     <div class="flex items-center justify-between p-4 mb-2">
+//             <div class="">
+//               <h4 class="text-[18px] text-[#111]">${data.name}</h4>
+//               <p class="text-[18px] text-[#5C5C5C]">${data.number}</p>
+//             </div>
+//             <p class="text-[18px] text-[#111]">${data.date}</p>
+//           </div>
+//     `;
+//     callHistoryContainer.appendChild(div);
+//   }
+
+function allCallBtn(id){
+    let callHistoryContainer = document.getElementById(id);
+  callHistoryContainer.innerText = "";
+  for (const data of callingData) {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <div class="flex items-center justify-between p-4 mb-2">
+            <div class="">
+              <h4 class="text-[18px] text-[#111]">${data.name}</h4>
+              <p class="text-[18px] text-[#5C5C5C]">${data.number}</p>
+            </div>
+            <p class="text-[18px] text-[#111]">${data.date}</p>
+          </div>
+    `;
+    callHistoryContainer.appendChild(div);
+  }
+}
+
+
